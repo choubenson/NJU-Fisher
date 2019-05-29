930e8e8600e971fa509a24f07a93fdff53fa0fbf
Debonet <719893090@qq.com>
2019-05-29 14:32:34 +0800

    完成时间

--- cloudfunctions/confirmTrade/index.js
+++ cloudfunctions/confirmTrade/index.js
@@ -1,18 +1,20 @@
 //此云函数用于确认商品交易成功，event.commodityId传入商品id
 const cloud = require('wx-server-sdk')
-cloud.init()
+cloud.init();
 const db = cloud.database();
+const _ = db.command;
 
 exports.main = async (event, context) => {
   try {
-    return await db.collection('shangpin').where({
+    var shangpin = await db.collection('shangpin').where({
       _id: event.commodityId
     })
-      .update({
-        data: {
-          state: 1
-        },
-      })
+    shangpin.update({
+      data: {
+        state: 1,
+        finishDate: new Date()
+      },
+    })
   } catch (e) {
     console.error(e)
   }
